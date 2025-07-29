93bde3ab6feeb6ca5adadfc1a9a492c4ddda77db
rohit-roshan123 <varrerohitroshan@gmail.com>
2025-07-30 03:08:20 +0530

    setting routes using react-router-dom

--- ApnaCart/apnacart/src/App.jsx
+++ ApnaCart/apnacart/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <div>
+        <Routes>
+          <Route></Route>
+          <Route></Route>
+          <Route></Route>
+        </Routes>
+      </div>
 
 
 
---
+++ ApnaCart/apnacart/src/components/MainBanner.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const MainBanner = () => {
+  return (
+    <div className='relative'>
+
+        <img src={assets.main_banner_bg} alt="" className='w-full hidden md:block'/>
+        <img src={assets.main_banner_bg_sm} alt="" className='w-full md:hiddenk'/>
+
+
+
+    </div>
+  )
+}
+
+export default MainBanner
